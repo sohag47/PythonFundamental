d35f5157b98f220de5decca1fe55548f28b207bf
sohag47 <minhazul.islamcse@gmail.com>
2024-01-08 21:42:10 +0600

    two sum

---
+++ CompetitiveProgramming/geeksforgeeks/basic/TwoSum.py
@@ -0,0 +1,17 @@
+"""
+1. Two Sum
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+"""
+
+
+def main():
+    target = input("Enter Target Number:")
+    nums = list(input("Enter Numbers:"))
+    print(type(nums))
+    print(nums)
+
+
+if __name__ == "__main__":
+    main()
